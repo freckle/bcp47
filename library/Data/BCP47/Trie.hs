637188171b1cca93181b4dc93799d9eaa8176c2f
Evan Rutledge Borden <eborden@frontrowed.com>
2019-05-01 17:26:20 -0500

    Add null check to Trie

--- library/Data/BCP47/Trie.hs
+++ library/Data/BCP47/Trie.hs
@@ -5,10 +5,11 @@ module Data.BCP47.Trie
   , lookup
   , union
   , unionWith
+  , null
   )
 where
 
-import Prelude hiding (lookup)
+import Prelude hiding (lookup, null)
 
 import Data.BCP47
 import Data.BCP47.Trie.Internal
@@ -17,3 +18,7 @@ import qualified Data.Map as Map
 -- | Lookup the most relevant item for a tag
 lookup :: BCP47 -> Trie a -> Maybe a
 lookup tag trie = lookup2 tag =<< Map.lookup (language tag) (unLanguage trie)
+
+-- | Check if a Trie is empty
+null :: Trie a -> Bool
+null = Map.null . unLanguage
