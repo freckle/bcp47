f9f0d8da94953fcd48138d2bfde863bd75a340dd
Chris Parks <christopher.daniel.parks@gmail.com>
2019-05-21 11:58:01 -0700

    Add ToField and FromField orphan instances in bcp47-cassava package

---
+++ bcp47-cassava/library/Data/BCP47/Csv.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Data.BCP47.Csv () where
+
+import Data.BCP47 (BCP47, fromText, toText)
+import Data.Csv (FromField(..), ToField(..))
+import Data.Text (unpack)
+
+instance ToField BCP47 where
+  toField = toField . toText
+
+instance FromField BCP47 where
+  parseField bytes = do
+    text <- parseField bytes
+    either (fail . unpack) pure $ fromText text
