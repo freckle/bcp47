be77d3080250526fdd3e3a884f29d51d0c78e445
Chris Parks <christopher.daniel.parks@gmail.com>
2019-05-21 13:42:45 -0700

    Document toText and add regression test for parsing

--- bcp47/tests/Data/BCP47Spec.hs
+++ bcp47/tests/Data/BCP47Spec.hs
@@ -11,24 +11,35 @@ import Data.BCP47.Internal.Variant
 import Data.ISO3166_CountryCodes (CountryCode(CN))
 import Data.LanguageCodes (ISO639_1(ZH))
 import qualified Data.Set as Set
-import Data.Text (pack, unpack)
+import Data.Text (unpack)
 import Test.Hspec
 import Test.QuickCheck (property)
 
 spec :: Spec
 spec = do
-  describe "fromText" $ it "parses all components" $ do
-    lng <- either (ioError . userError . unpack) pure
-      $ fromText "zh-abc-def-zxy-Hant-CN-1967-y-extensi-x-private1-private2"
-    language lng `shouldBe` ZH
-    extendedLanguageSubtags lng
-      `shouldBe` Set.singleton (LanguageExtension "abc-def-zxy")
-    script lng `shouldBe` Just (Script "Hant")
-    region lng `shouldBe` Just CN
-    variants lng `shouldBe` Set.singleton (Variant "1967")
-    extensions lng `shouldBe` Set.singleton (Extension "y-extensi")
-    privateUse lng
-      `shouldBe` Set.fromList [PrivateUse "private1", PrivateUse "private2"]
+  describe "fromText" $ do
+    it "parses all components" $ do
+      lng <- either (ioError . userError . unpack) pure
+        $ fromText "zh-abc-def-zxy-Hant-CN-1967-y-extensi-x-private1-private2"
+      language lng `shouldBe` ZH
+      extendedLanguageSubtags lng
+        `shouldBe` Set.singleton (LanguageExtension "abc-def-zxy")
+      script lng `shouldBe` Just (Script "Hant")
+      region lng `shouldBe` Just CN
+      variants lng `shouldBe` Set.singleton (Variant "1967")
+      extensions lng `shouldBe` Set.singleton (Extension "y-extensi")
+      privateUse lng
+        `shouldBe` Set.fromList [PrivateUse "private1", PrivateUse "private2"]
+
+    it "only parses complete specifiers" $ do
+      -- Specifically, the region CN should not be parsed out of the variant CNUVWXYX
+      lng <- either (ioError . userError . unpack) pure $ fromText "zh-CNUVWXYX"
+      language lng `shouldBe` ZH
+      extendedLanguageSubtags lng `shouldBe` Set.empty
+      script lng `shouldBe` Nothing
+      region lng `shouldBe` Nothing
+      variants lng `shouldBe` Set.singleton (Variant "CNUVWXYX")
+      privateUse lng `shouldBe` Set.empty
 
   describe "Arbitrary"
     . it "can parse arbitrary generated tags"
