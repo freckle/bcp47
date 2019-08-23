e9f5848055e017c1baf96bb30d036a7cbc7c0a88
Evan Rutledge Borden <eborden@frontrowed.com>
2019-08-23 12:35:32 -0500

    Add failure message for region parser
    Alternative parsers can provide confusing results. Instead we can
    provide better context with a `<?>` operator. Doctest have been added to
    confirm this.

--- bcp47/library/Data/BCP47/Internal/Region.hs
+++ bcp47/library/Data/BCP47/Internal/Region.hs
@@ -12,7 +12,7 @@ import Data.BCP47.Internal.Parser (complete)
 import Data.Bifunctor (first)
 import Data.Text (Text, pack)
 import Data.Void (Void)
-import Text.Megaparsec (Parsec, count, parse, try)
+import Text.Megaparsec (Parsec, count, parse, try, (<?>))
 import Text.Megaparsec.Char (digitChar, upperChar)
 import Text.Megaparsec.Error (errorBundlePretty)
 import Text.Read (readEither)
@@ -21,6 +21,16 @@ regionToText :: Country -> Text
 regionToText = alphaTwoUpper
 
 -- | Parse a region subtag from 'Text'
+--
+-- >>> regionFromText $ pack "ZW"
+-- Right zimbabwe
+--
+-- >>> regionFromText $ pack "012"
+-- Right algeria
+--
+-- >>> regionFromText $ pack "asdf"
+-- Left "regionFromText:1:1:\n  |\n1 | asdf\n  | ^\nunexpected 'a'\nexpecting 2 or 3 character country code\n"
+--
 regionFromText :: Text -> Either Text Country
 regionFromText =
   first (pack . errorBundlePretty) . parse regionP "regionFromText"
@@ -33,7 +43,7 @@ regionFromText =
 -- @@
 --
 regionP :: Parsec Void Text Country
-regionP = complete $ try alpha2 <|> num3
+regionP = complete (try alpha2 <|> num3 <?> "2 or 3 character country code")
  where
   alpha2 =
     maybe (fail "Invalid 2 character country code") pure
