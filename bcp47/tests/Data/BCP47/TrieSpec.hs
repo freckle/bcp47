7650f4a1e06adc84c3bdcdae6d6de97a6c5b6f09
Evan Rutledge Borden <eborden@frontrowed.com>
2020-08-24 11:54:38 -0500

    Use a mono-bind instead of overloaded lists

--- bcp47/tests/Data/BCP47/TrieSpec.hs
+++ bcp47/tests/Data/BCP47/TrieSpec.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedLists #-}
-
 module Data.BCP47.TrieSpec
   ( spec
   ) where
@@ -30,31 +28,31 @@ spec = do
       lookup tag (singleton tag "string") `shouldBe` Just "string"
 
     it "lookups no match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       lookup es trie `shouldBe` Nothing
 
     it "lookups no match deeply" $ do
-      let trie = fromNonEmpty [(enGBTJP, "colour")]
+      let Just trie = fromList [(enGBTJP, "colour")]
       lookup enGB trie `shouldBe` Nothing
 
     it "lookups an exact match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       lookup en trie `shouldBe` Just "color"
 
     it "lookups on just language" $ do
-      let trie = fromNonEmpty [(en, "color"), (es, "colour")]
+      let Just trie = fromList [(en, "color"), (es, "colour")]
       lookup es trie `shouldBe` Just "colour"
 
     it "lookups a deep exact match" $ do
-      let trie = fromNonEmpty [(enGBTJP, "foo"), (enGB, "colour")]
+      let Just trie = fromList [(enGBTJP, "foo"), (enGB, "colour")]
       lookup enGBTJP trie `shouldBe` Just "foo"
 
     it "lookups a relevant match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       lookup enTJP trie `shouldBe` Just "color"
 
     it "lookups a deep relevant match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       lookup enGBTJP trie `shouldBe` Just "colour"
 
   describe "match" $ do
@@ -62,29 +60,29 @@ spec = do
       match tag (singleton tag "string") `shouldBe` Just "string"
 
     it "matches no match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       match es trie `shouldBe` Nothing
 
     it "matches no match deeply" $ do
-      let trie = fromNonEmpty [(enGBTJP, "colour")]
+      let Just trie = fromList [(enGBTJP, "colour")]
       match enGB trie `shouldBe` Nothing
 
     it "matches an exact match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       match en trie `shouldBe` Just "color"
 
     it "matches on just language" $ do
-      let trie = fromNonEmpty [(en, "color"), (es, "colour")]
+      let Just trie = fromList [(en, "color"), (es, "colour")]
       match es trie `shouldBe` Just "colour"
 
     it "matches a deep exact match" $ do
-      let trie = fromNonEmpty [(enGBTJP, "foo"), (enGB, "colour")]
+      let Just trie = fromList [(enGBTJP, "foo"), (enGB, "colour")]
       match enGBTJP trie `shouldBe` Just "foo"
 
     it "matches a relevant match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       match enTJP trie `shouldBe` Nothing
 
     it "matches a deep relevant match" $ do
-      let trie = fromNonEmpty [(en, "color"), (enGB, "colour")]
+      let Just trie = fromList [(en, "color"), (enGB, "colour")]
       match enGBTJP trie `shouldBe` Nothing
