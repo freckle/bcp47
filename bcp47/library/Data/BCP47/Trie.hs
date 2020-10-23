980bc8a0c50c0fb2f6af3e397e488ec036c32fee
Evan Rutledge Borden <eborden@frontrowed.com>
2020-10-23 09:55:16 -0500

    Add more clarity to lookup function docs
    The difference between `lookup` and `match` was not clearly stated. This
    adds more robust docs to `lookup`, paraphrasing and pointing to the
    relevant ietf text.

--- bcp47/library/Data/BCP47/Trie.hs
+++ bcp47/library/Data/BCP47/Trie.hs
@@ -24,6 +24,14 @@ import qualified Data.Map as Map
 import Data.Maybe (isJust)
 
 -- | Lookup the most relevant item for a tag
+--
+-- "Lookup is used to select the single language tag that best matches the
+-- language priority list for a given request...For example, if the language
+-- range is 'de-ch', a lookup operation can produce content with the tags 'de'
+-- or 'de-CH' but never content with the tag 'de-CH-1996'."
+--
+-- https://tools.ietf.org/html/bcp47#page-2-12
+--
 lookup :: BCP47 -> Trie a -> Maybe a
 lookup tag trie = lookup2 tag =<< Map.lookup (language tag) (unLanguage trie)
 
