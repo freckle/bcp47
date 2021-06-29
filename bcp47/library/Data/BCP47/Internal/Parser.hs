25ac8630fc934c70a4065d6db43cf585b3a6ed05
Chris Parks <christopher.daniel.parks@gmail.com>
2021-06-29 16:33:29 -0700

    Add custom parsers for case-insensitive ASCII letters and digits

--- bcp47/library/Data/BCP47/Internal/Parser.hs
+++ bcp47/library/Data/BCP47/Internal/Parser.hs
@@ -1,12 +1,16 @@
 module Data.BCP47.Internal.Parser
   ( complete
+  , asciiLetterDigit
+  , asciiLetter
+  , asciiDigit
   ) where
 
 import Control.Applicative ((<|>))
 import Control.Monad (void)
 import Data.Text (Text)
+import Data.Char (ord)
 import Data.Void (Void)
-import Text.Megaparsec (Parsec, eof, lookAhead, noneOf)
+import Text.Megaparsec (Parsec, eof, lookAhead, noneOf, satisfy, (<?>))
 import Text.Megaparsec.Char (char)
 
 -- | Ensure a subtag extends to the next '-' or end of input
@@ -24,3 +28,39 @@ complete parser =
 
 tagChars :: String
 tagChars = '-' : ['a' .. 'z'] <> ['A' .. 'Z'] <> ['0' .. '9']
+{-# NOINLINE tagChars #-}
+
+-- | Parse a single case-insensitive ASCII letter or digit
+asciiLetterDigit :: Parsec Void Text Char
+asciiLetterDigit = satisfy isAsciiLetterDigit <?> "ascii letter or digit"
+ where
+  isAsciiLetterDigit c = do
+    let code = ord c
+    isCodeAsciiUpper code || isCodeAsciiLower code || isCodeAsciiDigit code
+{-# INLINE asciiLetterDigit #-}
+
+-- | Parse a single case-insensitive ASCII letter
+asciiLetter :: Parsec Void Text Char
+asciiLetter = satisfy isAsciiLetter <?> "ascii letter"
+ where
+  isAsciiLetter c = do
+    let code = ord c
+    isCodeAsciiUpper code || isCodeAsciiLower code
+{-# INLINE asciiLetter #-}
+
+-- | Parse a single ASCII digit
+asciiDigit :: Parsec Void Text Char
+asciiDigit = satisfy (isCodeAsciiDigit . ord) <?> "ascii digit"
+{-# INLINE asciiDigit #-}
+
+isCodeAsciiUpper :: Int -> Bool
+isCodeAsciiUpper code = ord 'A' <= code && code <= ord 'Z'
+{-# INLINE isCodeAsciiUpper #-}
+
+isCodeAsciiLower :: Int -> Bool
+isCodeAsciiLower code = ord 'a' <= code && code <= ord 'z'
+{-# INLINE isCodeAsciiLower #-}
+
+isCodeAsciiDigit :: Int -> Bool
+isCodeAsciiDigit code = ord '0' <= code && code <= ord '9'
+{-# INLINE isCodeAsciiDigit #-}
