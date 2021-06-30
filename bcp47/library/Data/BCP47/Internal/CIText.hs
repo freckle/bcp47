a9f4ec62454cdcb19aa86212942cfc7789476969
Chris Parks <christopher.daniel.parks@gmail.com>
2021-06-29 17:19:54 -0700

    Add newtype for CI Text

---
+++ bcp47/library/Data/BCP47/Internal/CIText.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module Data.BCP47.Internal.CIText
+  ( CIText(..)
+  , fromText
+  , pack
+  , original
+  , foldedCase
+  ) where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.CaseInsensitive (CI)
+import Data.String (IsString)
+import qualified Data.CaseInsensitive as CI
+
+-- | Internal newtype for 'CI Text'
+--
+-- Constructor is exposed since this is an internal module, but ths
+-- interface may change. Module is meant to be imported qualified.
+--
+newtype CIText = CIText
+  { unCIText :: CI Text
+  }
+  deriving newtype (Eq, Show, Ord, IsString)
+
+-- | Convert 'Text' to 'CIText'
+fromText :: Text -> CIText
+fromText = CIText . CI.mk
+
+-- | Convert 'String' to 'CIText'
+pack :: String -> CIText
+pack = fromText . T.pack
+
+-- | Extract case-folded 'Text'
+foldedCase :: CIText -> Text
+foldedCase = CI.foldedCase . unCIText
+
+-- | Recover original 'Text'
+original :: CIText -> Text
+original = CI.original . unCIText
