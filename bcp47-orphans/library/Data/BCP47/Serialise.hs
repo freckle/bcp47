0453ae27eb36caa50821c1fd80b803e396d658f4
Tom Carstens <tom.carstens@renaissance.com>
2023-05-30 14:27:40 -0500

    use to/fromText as the whole implementation

--- bcp47-orphans/library/Data/BCP47/Serialise.hs
+++ bcp47-orphans/library/Data/BCP47/Serialise.hs
@@ -5,19 +5,10 @@ module Data.BCP47.Serialise () where
 import Codec.Serialise
 import Codec.Serialise.Decoding
 import Codec.Serialise.Encoding
-import Control.Monad (void)
 import Data.BCP47 (BCP47, fromText, toText)
 import Data.Text (unpack)
 
 instance Serialise BCP47 where
   -- bypass children not having their own Serialise instances by using toText/fromText
-  encode bcp = encodeListLen 2 <> encodeTag 0 <> encodeString (toText bcp)
-  decode = do
-    -- these two must be read to consume them
-    -- ignore them as they're here as part of the standard format but we don't need them
-    void decodeListLen
-    void decodeTag
-    textual <- decodeString
-    case fromText textual of
-      Left err -> fail . unpack $ err
-      Right val -> pure val
+  encode = encode . toText
+  decode = either (fail . unpack) pure . fromText =<< decode
