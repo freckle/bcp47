5b197abfd6c193773da0654ab6267e03b681f0b0
patrick brisbin <pbrisbin@gmail.com>
2025-07-09 14:15:24 -0400

    Fixup OpenApi instances
    - Write it by hand, to add appropriate metadata
    - Reuse `ToSchema` for `ToParamSchema`

--- bcp47-orphans/library/Data/BCP47/OpenApi.hs
+++ bcp47-orphans/library/Data/BCP47/OpenApi.hs
@@ -1,18 +1,30 @@
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.BCP47.OpenApi () where
 
-import Autodocodec (Autodocodec (..))
-import Autodocodec.OpenAPI.DerivingVia ()
+import Control.Lens ((&), (?~))
+import Data.Aeson (Value (..))
 import Data.BCP47
 import Data.BCP47.Autodocodec ()
+import Data.OpenApi (description, example, format, schema)
 import Data.OpenApi.ParamSchema (ToParamSchema (..))
-import Data.OpenApi.Schema (ToSchema (..))
+import Data.OpenApi.Schema (ToSchema (..), toSchema)
 import Data.Proxy (Proxy (..))
+import Data.Text (Text)
 
-deriving via (Autodocodec BCP47) instance ToSchema BCP47
+instance ToSchema BCP47 where
+  declareNamedSchema _ = do
+    -- Use the schema for Text, but add more details
+    textSchema <- declareNamedSchema $ Proxy @Text
+
+    pure
+      $ textSchema
+        & schema . description ?~ "BCP47 language tag"
+        & schema . example ?~ String "en"
+        & schema . format ?~ "bcp47"
 
 instance ToParamSchema BCP47 where
-  toParamSchema _ = toParamSchema (Proxy :: Proxy String)
+  toParamSchema _ = toSchema $ Proxy @Text
